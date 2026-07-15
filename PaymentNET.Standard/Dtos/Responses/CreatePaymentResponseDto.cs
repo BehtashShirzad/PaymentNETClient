ace97c236aa111b29286f2d1fbe999cf17d1de67
Mohamad <Mohamadshirzad81@gmail.com>
2026-07-15 22:02:15 +0330

    fx

--- PaymentNET.Standard/Dtos/Responses/CreatePaymentResponseDto.cs
+++ PaymentNET.Standard/Dtos/Responses/CreatePaymentResponseDto.cs
@@ -2,6 +2,15 @@
 {
     public class CreatePaymentResponseDto
     {
-        
+        public long PurchaseId{get;set;}
+        public string PurchaseIdStr{get;set;}
+        public string ClientReferenceNumber{get;set;}
+        public string PspSwitchingUrl{get;set;}
+        public string Fee{get;set;}
+        public long  AffiliateFee{get;set;}
+        public long  ShaparakFee{get;set;}
+        public long  NetAmount{get;set;}
+        public long  PayableAmount{get;set;}
+        public string  Currency{get;set;}
     }
 }
